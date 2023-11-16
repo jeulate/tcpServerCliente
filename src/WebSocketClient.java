73487908647333f00fecd8b531258a4aeb4188da
jeulate <juan_a_eulate@hotmail.com>
2023-11-16 19:57:36 -0400

    primera presentacion

--- src/WebSocketClient.java
+++ src/WebSocketClient.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class WebSocketClient {
+import java.io.*;
+import java.net.Socket;
+
+public class WebSocketClient {
+
+    public static void main(String[] args) {
+        String serverAddress = "localhost";
+        int serverPort = 8080;
+
+        try (Socket socket = new Socket(serverAddress, serverPort);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+            System.out.println("Conectado al servidor WebSocket en " + serverAddress + ":" + serverPort);
+
+            // Envia un mensaje al servidor
+            String message = "Hola, servidor WebSocket!";
+            out.println(message);
+            System.out.println("Mensaje enviado al servidor: " + message);
+
+            // Puedes recibir respuestas del servidor aquí
+            String response;
+            while ((response = in.readLine()) != null) {
+                System.out.println("Respuesta del servidor: " + response);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
--- src/WebSocketServer.java
+++ src/WebSocketServer.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class WebSocketServer {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WebSocketServer {
+    public static void main(String[] args) {
+        int port = 8080;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Servidor WebSocket en ejecución en el puerto " + port);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Cliente conectado desde " + clientSocket.getInetAddress().getHostAddress());
+
+                WebSocketHandler handler = new WebSocketHandler(clientSocket);
+                handler.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
+
+
+
+class WebSocketHandler extends Thread {
+    private final Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public WebSocketHandler(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    public void run() {
+        try {
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Mensaje recibido del cliente: " + inputLine);
+                // Puedes procesar los mensajes aquí y enviar respuestas si es necesario.
+
+            }
+
+
+            in.close();
+            out.close();
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
